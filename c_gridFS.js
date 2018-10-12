ed47a0a48518bf53112c02234d096a35475e6310
kapoor-rakshit <kapoor_rakshit@yahoo.co.in>
2018-10-13 00:24:09 +0530

    pro

---
+++ c_gridFS.js
@@ -0,0 +1,62 @@
+
+var mongoose = require("mongoose");
+var grid = require("gridfs-stream");
+var fs = require("fs");
+
+grid.mongo = mongoose.mongo;
+
+mongoose.Promise = global.Promise;
+
+var conn = mongoose.connect("mongodb://localhost:27017/menagerie", {useMongoClient: true});
+
+conn.on("error", function(err){
+	console.log(err.code, err.message);
+});
+
+conn.once("open", function(){
+	console.log("CONNECTED to DB");
+
+	var gfs = grid(conn.db);
+
+
+	                       /* WRITING to gridfs */
+
+  //   var writestream = gfs.createWriteStream({filename: "rakshit_MONGO.jpg"});   // FILENAME as to store in DB
+  //   fs.createReadStream("C:\\Users\\ra20024024\\Desktop\\Rakshit_docs\\rakshit new photo.jpg").pipe(writestream);
+
+  //   writestream.on("close", function(file){
+  //   	 console.log(file.filename, "Written to MONGOdb");
+
+  //   	 conn.close(function(){
+  // 	    console.log("CONNECTION closed");
+  //      });
+  //   });
+
+
+    					 /* READING from MONGOdb GRIDfs */
+
+    // var fs_write_stream = fs.createWriteStream("rakshit_from_MONGO.jpg");      // FILENAME as to store in MACHINE
+    // gfs.createReadStream({filename: "rakshit_MONGO.jpg"}).pipe(fs_write_stream);
+
+    // fs_write_stream.on("close", function(){
+    // 	 console.log("Written to MACHINE");
+
+    // 	 conn.close(function(){
+    // 		console.log("CONNECTION closed");
+    // 	  });
+    // });
+
+
+    					/* DELETE from MONGO GRIDfs*/
+
+    gfs.remove({filename: "rakshit_MONGO.jpg"}, function(err, gridStore){
+    	if(err) console.log(err.code, err.message);
+    	else console.log("FILE deleted");
+
+    	conn.close(function(){
+    		console.log("CONNECTION closed");
+    	});
+    });
+
+});
+
